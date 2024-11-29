854a4fd78c7de9d2d8021ccdb5ff0c7e9ad723fa
Stavros Hadjicostis <106922068+StavrosHadjicostis@users.noreply.github.com>
2024-11-29 14:13:54 +0200

    Added protection to the home page so it can be accessible only be logged in users
    Also fully implemented the log in and register

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-// import "./login.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAGKBVwIF4BSqsxzHzhaVjdXAGNG2wShpI",
+    authDomain: "skillscyprus-24ccf.firebaseapp.com",
+    projectId: "skillscyprus-24ccf",
+    storageBucket: "skillscyprus-24ccf.firebasestorage.app",
+    messagingSenderId: "331801842174",
+    appId: "1:331801842174:web:b0addc50b7beb9520ab0af",
+    measurementId: "G-VZVEKNW7QJ"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+// const auth = getAuth();
+
+onAuthStateChanged(auth, (user) => {
+    console.log(user)
+  if (!user) {
+    // If not authenticated, redirect to login
+    window.location.href = "login.html";
+  }
+});
+
+// document.getElementById("logout").addEventListener("click", async () => {
+//   try {
+//     await auth.signOut();
+//     window.location.href = "login.html"; // Redirect to login
+//   } catch (error) {
+//     console.error("Error logging out:", error.message);
+//   }
+// });
